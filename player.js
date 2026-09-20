64f9063d4cc256f57e8e9b82ac41da795694869c
yeonjiho <yeonjiho665@gmail.com>
2026-09-20 22:01:01 +0900

    Update player.js

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ const players = {
 
   "시은": {
     number: "PLAYER 01",
-    last_rank: "1위",
+    last_rank: "미참",
     wins: "3회",
     finals: "3회",
     seasons: "6",
@@ -12,7 +12,7 @@ const players = {
 
   "지호": {
     number: "PLAYER 02",
-    last_rank: "1위",
+    last_rank: "2위",
     wins: "2회",
     finals: "3회",
     seasons: "7",
@@ -22,7 +22,7 @@ const players = {
 
   "서한": {
     number: "PLAYER 03",
-    last_rank: "2위",
+    last_rank: "미참",
     wins: "0회",
     finals: "0회",
     seasons: "5",
@@ -32,7 +32,7 @@ const players = {
 
   "민준": {
     number: "PLAYER 04",
-    last_rank: "2위",
+    last_rank: "4위",
     wins: "0회",
     finals: "0회",
     seasons: "7",
@@ -42,7 +42,7 @@ const players = {
 
   "민성": {
     number: "PLAYER 05",
-    last_rank: "3위",
+    last_rank: "5위",
     wins: "0회",
     finals: "0회",
     seasons: "7",
@@ -52,7 +52,7 @@ const players = {
 
   "서빈": {
     number: "PLAYER 06",
-    last_rank: "1위",
+    last_rank: "6위",
     wins: "1회",
     finals: "1회",
     seasons: "5",
@@ -82,7 +82,7 @@ const players = {
 
   "혜영": {
     number: "PLAYER 09",
-    last_rank: "5위",
+    last_rank: "7위",
     wins: "0회",
     finals: "0회",
     seasons: "5",
